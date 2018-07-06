cbc2cb67aba2114c0ccdbc0695cd6681647be5f9
chandu <chandrasr.palle@gmail.com>
2018-07-06 11:03:25 +0200

    Port changed again

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 
@@ -11,6 +12,6 @@ app.get("/about", function(req, res){
     res.render("about");
 });
 
-app.listen("3000" || process.env.PORT, function(){
-    console.log("Server Started at Port: 3000");
+app.listen(PORT, function(){
+    console.log("Server Started!!");
 });
